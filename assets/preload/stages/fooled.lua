b86b2ac9aa8287a5506483619770fc8649c64d9d
gabzsa <109770528+gabzsa@users.noreply.github.com>
2022-09-17 09:01:46 -0300

    Add files via upload

---
+++ assets/preload/stages/fooled.lua
@@ -0,0 +1,25 @@
+--made with Super_Hugo's Stage Editor
+
+function onCreate()
+
+	makeLuaSprite('obj1', 'Fooled/DOORMario_HouseStage', -845, 208)
+
+	setObjectOrder('obj1', 2)
+
+	addLuaSprite('obj1', true)
+
+
+	makeLuaSprite('obj2', 'Fooled/DOORMario_OutsideStage', -399, 274)
+
+	setObjectOrder('obj2', 1)
+
+	addLuaSprite('obj2', true)
+
+
+	makeLuaSprite('obj3', 'Fooled/DOORMario_SofaStage', -743, 644)
+
+	setObjectOrder('obj3', 4)
+
+	addLuaSprite('obj3', true)
+
+end
